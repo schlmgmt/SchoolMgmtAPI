01ec06c6a45eeb518c61f4bcda317e613f3df891
Dev <tekwanidev03@gmail.com>
2025-08-30 01:32:49 +0530

    Remove min length from loginviewmodel

--- SchoolMgmtAPI/Models/ViewModel/LoginViewModel.cs
+++ SchoolMgmtAPI/Models/ViewModel/LoginViewModel.cs
@@ -8,7 +8,6 @@ namespace SchoolMgmtAPI.Models.ViewModel
         [EmailAddress]
         public string Email { get; set; }
         [Required]
-        [MinLength(8)]
         public string Password { get; set; }
     }
 }
